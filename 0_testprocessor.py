b39976cd0eeb8fcfafb860c06d4936fe246c7c52
Jiwant Singh <jiwantsingh01@gmail.com>
2019-08-09 21:54:42 -0400

    Adding further test python files

--- 0_testprocessor.py
+++ 0_testprocessor.py
@@ -3,5 +3,6 @@ import psutil
 import time
 
 for i in range(1, 100):
-    print(psutil.cpu_percent(), psutil.virtual_memory(), psutil.Process(os.getpid()))
+    #print(psutil.cpu_percent(), psutil.virtual_memory(), psutil.virtual_memory()[1], psutil.virtual_memory()[3], psutil.Process(os.getpid()))
+    print(f'Cpu Percent Usage={psutil.cpu_percent()} ,Virtual Memory={psutil.virtual_memory()[1]}, Free Memory={psutil.virtual_memory()[3]}')
     time.sleep(2)
